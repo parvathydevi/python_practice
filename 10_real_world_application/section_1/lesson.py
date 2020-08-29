727080c80bf70e37bd8a94258dac859fc6643872
U-RMA\sveeramani <sveeramani@sveeramani-3.rma.lan>
2020-08-29 16:32:58 +0530

    Added the section of the 10_real_world_application python udemy course

---
+++ 10_real_world_application/section_1/lesson.py
@@ -0,0 +1,5 @@
+import datetime
+print("The date and time is")
+print(datetime.datetime.now())
+
+print("The date and time is",datetime.datetime.now())
---
+++ 10_real_world_application/section_2/lesson.py
@@ -0,0 +1,24 @@
+import datetime
+print("The date and time is",datetime.datetime.now())
+
+print("The date and time is",datetime.datetime.now())
+mynow = datetime.datetime.now()
+print(mynow)
+
+
+mynumber = 10
+mytext = "Hello"
+print(mynumber,mytext)
+
+
+x = 10
+y = '10'
+z = 10.1
+
+sum1 = x + x
+sum2 = y + y
+print(sum1,sum2)
+print(type(x),type(y),type(z))
+
+x = [5,6,7]
+mysort = sort(x)
