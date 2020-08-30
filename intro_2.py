0587ad0728df126d9620aec09fb448c472a04e93
parvathydevi <prvthy7@gmail.com>
2020-08-30 04:57:41 +0100

    Synching all the files

---
+++ intro_2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug  9 19:34:55 2020
+
+@author: selva
+"""
+
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+
+for x in thisdict:
+  print(x) 
+
+print (thisdict["brand"])
+
+print (thisdict["model"])
+
+
+
+
