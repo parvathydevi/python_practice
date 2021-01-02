d26826e0a96288439d8187ca5dfecbf949fcd747
parvathy <prvthy7@gmail.com>
2021-01-02 13:05:22 +0530

    Added comments

--- fin_calc/fin_calc.py
+++ fin_calc/fin_calc.py
@@ -1,3 +1,8 @@
+
+"""
+ppf account 
+"""
+
 selva = 5000
 devi = 2000
 mukil = 1000
@@ -6,10 +11,15 @@ sum_ppf = selva+devi+mukil+akhil
 
 print('Sum = {4}'.format(selva, devi, mukil, akhil, sum_ppf))
       
- 
+"""
+nps account 
+""" 
 selva_nps = 5000
 print('NPS = {0}'.format(selva_nps))
 
+"""
+mf account 
+"""
 
 selva_mf = 20000
 mukil_mf = 8000
@@ -19,10 +29,21 @@ sum_mf = selva_mf+mukil_mf+akhil_mf+devi_mf
 
 print('sum_mf = {4}'.format(selva_mf,mukil_mf,akhil_mf,devi_mf,sum_mf))
 
-
+"""
+monthly cumulative  
+"""
 
 sum_tpm = sum_ppf+selva_nps+sum_mf
 print('Total per month = {3}'.format(sum_ppf,selva_nps,sum_mf,sum_tpm))
 
+
+"""
+yearly cumulative  
+"""
+
+
 mul = sum_tpm*12
 print('yearly = {0}'.format(mul))
+
+
+
